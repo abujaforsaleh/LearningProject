6cfac388b7dc4a78cbdff6e189ec2a024b76b758
pulokbd <abujafo34@gmail.com>
2023-02-27 02:30:33 +0600

    recycler view page moved to recyclerview activity

--- app/src/main/java/com/bauet/learningproject/MainActivity.java
+++ app/src/main/java/com/bauet/learningproject/MainActivity.java
@@ -18,36 +18,10 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<RecyclerViewModel> dataListForRecyclerView = new ArrayList<>();
-    //ArrayList<Integer> imageList = new ArrayList<>();
-    RecyclerView myRecyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpRecyclerModels();
 
-        myRecyclerView = findViewById(R.id.recyclerView);
-        MyRecyclerViewAdapter adapter = new MyRecyclerViewAdapter(this, dataListForRecyclerView);
-        myRecyclerView.setAdapter(adapter);
-        //myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        myRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-
-    }
-
-    private void setUpRecyclerModels(){
-
-        String[] placeName = getResources().getStringArray(R.array.place_names);
-        String[] placeCountry = getResources().getStringArray(R.array.place_country);
-        String[] placeLocation = getResources().getStringArray(R.array.place_location);
-        int length = placeName.length;
-        for(int i = 0;i<length;i++){
-            String imageName = placeName[i].replace(" ", "_").replace("-", "_").toLowerCase();
-            //imageList.add(getResources().getIdentifier(imageName, "drawable", getPackageName()));
-            int id = getResources().getIdentifier(imageName, "drawable", getPackageName());
-            dataListForRecyclerView.add(new RecyclerViewModel(placeName[i], placeCountry[i], placeLocation[i], id));
-
-        }
     }
 }
